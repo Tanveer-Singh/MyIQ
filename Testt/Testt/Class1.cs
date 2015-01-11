19ac8368fee6a4c2ee993bc32d616342272f2b0a
Rockky <devil_rules_universe@yahoo.co.in>
2015-01-11 19:31:22 +0530

    Update
    Update

--- Testt/Testt/Class1.cs
+++ Testt/Testt/Class1.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Testt
 {
-    public class Class1
+    public class A
     {
+
+        public void PrintMe()
+        {
+            Console.WriteLine("Printing");
+        }
+    }
+
+
+    public class B : A
+    {
+        public void PrintMe()
+        {
+            Console.WriteLine("Check");
+        }
+
+        public void PrintMe(string a)
+        {
+            Console.WriteLine("Printging");
+        }
+ 
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            B ob = new B();
+        }
     }
 }
